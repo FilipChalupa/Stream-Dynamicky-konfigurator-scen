561f38efcadc93b547a766ad962ce0f8d963001d
Filip Chalupa <chalupa.filip@gmail.com>
2020-10-01 20:29:29 +0200

    Server: test body parser

--- express/server.js
+++ express/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const serverless = require('serverless-http')
+const bodyParser = require('body-parser')
 
 exports.handler = function (event, context, callback) {
 	callback(null, {
