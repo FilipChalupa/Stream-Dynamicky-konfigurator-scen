fc869ab021985b1a8041431df1fc2a64303c86e7
Filip Chalupa <chalupa.filip@gmail.com>
2020-10-01 20:27:58 +0200

    Server: test only serverless

--- express/server.js
+++ express/server.js
@@ -1,8 +1,5 @@
 const express = require('express')
 const serverless = require('serverless-http')
-const bodyParser = require('body-parser')
-const { GoogleSpreadsheet } = require('google-spreadsheet')
-const app = express()
 
 exports.handler = function (event, context, callback) {
 	callback(null, {
